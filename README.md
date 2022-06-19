68fa644288f54e916fa503e5e8ad3ee67fa624e3
Gérald Maréchal <gerald.marechal@cronoseuropa.com>
2022-06-19 23:55:58 +0200

    OK amontrer à Louis

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # letter_speak
+
 A small program with nCurses and eSpeak to echo the letter pressed on the keyboard.
+
+1. install espeak
+2. install figlet
--- letterspeak.c
+++ letterspeak.c
@@ -5,13 +5,34 @@
 
 int main(void)
 {
-  initscr();                 /* Start curses mode 		  */
-  start_color();
-  printw("Hello World !!!"); /* Print Hello World		  */
-  printf("")
-  refresh();                 /* Print it on to the real screen */
-  getch();                   /* Wait for user input */
-  endwin();                  /* End curses mode		  */
+  FILE *fp;
+  char str[80];
+  char str2[80];
+  char screen[2000];
+  int character = 0;
 
+  initscr(); /* Start curses mode 		  */
+  start_color();
+
+  bool hasColors = has_colors();
+  bool canChangeColor = can_change_color();
+  printw("The terminal has color        : %d \n", hasColors);
+  printw("The user can modify the color : %d \n", canChangeColor);
+  refresh(); /* Print it on to the real screen */
+  while (true)
+  {
+    character = getch(); /* Wait for user input */
+    clear();
+    sprintf(str, "espeak -v fr-br '%c'", character);
+    sprintf(str2, "figlet -c '%c'", character);
+    fp = popen(str2, "r");
+    while (fgets(screen, sizeof(screen), fp) != NULL)
+    {
+      printw("%s", screen);
+    }
+    system(str);
+    refresh();
+  }
+  endwin(); /* End curses mode		  */
   return 0;
 }
